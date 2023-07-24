488dd2597e6b1dd2bd7730c355512781e848064d
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 16:37:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function remove() {
+let color = document.getElementsByTagName('select');
+let removeColor = color[0].value;
+let options = document.getElementsByTagName('option');
+console.log(removeColor);
+for(let i = 0;i<options.length;i++){
+	let currColor = options[i].value;
+    console.log(currColor);
+	if(currColor==removeColor){
+        let deleteNode = options[i];
+		deleteNode.remove();
+	}
+ }
+}
